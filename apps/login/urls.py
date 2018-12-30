da41883c3837c7484d6886eabc8dc8cdcbabe902
Danny <yanez.daniel0101@gmail.com>
2018-12-30 14:45:09 -0600

    First Commit

---
+++ apps/login/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="landing"),
+    url(r'^reserve',views.reserve, name="dashboard"),
+    url(r'^register$', views.register, name="register"),
+    url(r'^login$', views.login, name="login"),
+    url(r'^logout$', views.logout, name="logout")
+]
+
+
+
