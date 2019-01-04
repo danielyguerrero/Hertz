60e089d598408fa5dc8bb98b10220ba3cf65e7ce
Danny <yanez.daniel0101@gmail.com>
2019-01-03 22:01:16 -0600

    updated forms

--- apps/reservation/urls.py
+++ apps/reservation/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 urlpatterns = [
 
-	url(r'^$', views.index),
+	url(r'^$', views.index, name= 'dashboard'),
+	url(r'reservation/client',views.add_client, name='add_client'),
+	url(r'reservation/vehicle',views.vehicle, name='vehicle'),
+	url(r'reservation/location',views.location, name='location'),
+
 
 ]
+
+
+
--- apps/reservation/views.py
+++ apps/reservation/views.py
@@ -1,8 +1,49 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, redirect, reverse
+from .models import Reservation 
 
-# Create your views here.
+
+
+# ==============================================================
+# 						RENDER
+# ==============================================================
+
 def index(request):
 	return render(request, 'reservation/index.html')
+
+
+def vehicle(request):
+	return render(request, 'reservation/vehicle')
+
+
+# =================================================
+#                       PROCESS
+# =================================================
+
+def show_trip(request, trip_id):
+	trip = Trip.objects.get(id = trip_id)
+	context = {
+		'users': trip.joined_by.all(),
+		'trip': trip
+	}
+	return render(request, 'travelplanner/trip.html', context)
+
+
+def add_client(request):
+	if request.method == "POST":
+		errors = Trip.objects.validate(request.POST)
+
+		if not errors:
+			trip = Trip.objects.add_trip(request.POST, request.session["user_id"])
+			trip.joined_by.add(current_user(request))
+			print trip
+			return redirect(reverse('dashboard'))
+
+		flash_errors(request, errors)
+		return redirect(reverse('add_trip'))
+	else:
+		return render(request, 'travelplanner/add.html')
+
+
+
